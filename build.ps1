0809d5b755a3e21fffc66d920f190aaf12bdeeeb
Kunal Bajpai <Kunal.Bajpai@gmail.com>
2024-12-08 12:14:04 -0500

    Added PowerShell

---
+++ build.ps1
@@ -0,0 +1,20 @@
+#!/usr/bin/env pwsh
+
+# Create directory if it doesn't exist
+if (!(Test-Path -Path "dist")) {
+    New-Item -ItemType Directory -Path "dist"
+}
+
+# Build
+pdflatex resume.tex
+htlatex resume.tex
+# pandoc -o README.md resume.tex
+# pandoc -s resume.tex -o resume.docx
+
+# Copy files
+Copy-Item -Path "resume.pdf"    -Destination "dist/resume_kbajpai.pdf"
+Copy-Item -Path "resume.pdf"    -Destination "$env:USERPROFILE\Dropbox\Resume\resume_kbajpai.pdf"
+Copy-Item -Path "resume.html"   -Destination "index.html"
+
+# Clean git repository
+git clean -d -fx -f
