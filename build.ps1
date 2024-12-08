9f44820d8842bde0174e715484444f66d94ffaeb
Kunal Bajpai <Kunal.Bajpai@gmail.com>
2024-12-08 17:00:52 -0500

    2024-12-08 C

--- build.ps1
+++ build.ps1
@@ -1,20 +1,18 @@
 #!/usr/bin/env pwsh
 
-# Create directory if it doesn't exist
-if (!(Test-Path -Path "dist")) {
-    New-Item -ItemType Directory -Path "dist"
-}
-
 # Build
 pdflatex resume.tex
 htlatex resume.tex
 pandoc -o README.md resume.tex
-# pandoc -s resume.tex -o resume.docx
+pandoc -s resume.tex -o resume.docx
+
+$timestamp = Get-Date -Format "yyyyMMdd"
 
 # Copy files
-Copy-Item -Path "resume.pdf"    -Destination "dist/resume_kbajpai.pdf"
-Copy-Item -Path "resume.pdf"    -Destination "$env:USERPROFILE\Dropbox\Resume\resume_kbajpai.pdf"
 Copy-Item -Path "resume.html"   -Destination "index.html"
+Copy-Item -Path "resume.pdf"    -Destination "$env:USERPROFILE\Dropbox\Resume\Resume.Kunal.Bajpai.$timestamp.pdf"
+Copy-Item -Path "resume.html"   -Destination "$env:USERPROFILE\Dropbox\Resume\Resume.Kunal.Bajpai.$timestamp.html"
+Copy-Item -Path "resume.docx"   -Destination "$env:USERPROFILE\Dropbox\Resume\Resume.Kunal.Bajpai.$timestamp.docx"
 
 # Clean git repository
 git clean -d -fx -f
